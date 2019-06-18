693cf5cebaf8dec7ae2db014d4259165a8cb8eb2
meska <meskatech@gmail.com>
2019-06-18 14:55:22 +0200

    fix

--- mjpegsw.py
+++ mjpegsw.py
@@ -12,7 +12,6 @@ from socketserver import ThreadingMixIn
 import cv2
 
 from PIL import Image
-from netifaces import ifaddresses, AF_INET
 
 capture = None
 
@@ -64,7 +63,8 @@ def handle_args():
     parser = argparse.ArgumentParser(description='Mjpeg streaming server: mjpegsw -p 8080 --camera 2')
     parser.add_argument('-p', '--port', help='http listening port, default 5001', type=int, default=5001)
     parser.add_argument('-c', '--camera', help='opencv camera number, ex. -c 1', type=int, default=0)
-    parser.add_argument('-i', '--interface', help='network interface, default en0', type=str, default='en0')
+    parser.add_argument('-i', '--ipaddress', help='listening ip address, default localhost', type=str,
+                        default='localhost')
     parser.add_argument('-s', '--size', help='set size', nargs=2, type=int, default=(320, 240))
 
     params = vars(parser.parse_args())
@@ -76,12 +76,11 @@ def main():
     global capture
 
     params = handle_args()
-    ip_addr = ifaddresses(params['interface'])[AF_INET][0]['addr']
     capture = cv2.VideoCapture(params['camera'])
-    server = ThreadedHTTPServer((ip_addr, params['port']), CamHandler)
+    server = ThreadedHTTPServer((params['ipaddress'], params['port']), CamHandler)
 
     try:
-        print(f"Mjpeg server started on http://{ip_addr}:{params['port']}")
+        print(f"Mjpeg server started on http://{params['ipaddress']}:{params['port']}")
         server.serve_forever()
     except KeyboardInterrupt:
         capture.release()
